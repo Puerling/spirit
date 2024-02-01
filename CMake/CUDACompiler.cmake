eedf7bb5b38d22273416370c78f08d936d40f452
puerling <t.puerling@fz-juelich.de>
2024-02-01 09:54:30 +0100

    modernize setting CUDA compiler flags

--- CMake/CUDACompiler.cmake
+++ CMake/CUDACompiler.cmake
@@ -1,33 +1,36 @@
 ######### CUDA setup ###############################################
 enable_language( CUDA )
 
-find_package( CUDA 8 REQUIRED )
+find_package( CUDAToolkit 11 REQUIRED )
 
-# add_compile_options($<COMPILE_LANGUAGE:CUDA>:)
-if( "${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA" ) 
-    set( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -lineinfo -arch=sm_${SPIRIT_CUDA_ARCH} --expt-relaxed-constexpr --expt-extended-lambda" )
-    ### Deactivate CUDA warning inside Eigen such as "warning: __host__ annotation is ignored on a function("Quaternion") that is explicitly defaulted on its first declaration"
-    set( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} --diag-suppress=20011 --diag-suppress=20012 --diag-suppress=20014 --diag-suppress=1675")
-    ### Display warning number when writing a warning
-    set( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} --display-error-number" )
-
-    set( META_COMPILER         "${META_COMPILER} and nvcc" )
-    set( META_COMPILER_VERSION "${META_COMPILER_VERSION} and ${CUDA_VERSION}" )
-    set( META_COMPILER_FULL    "${META_COMPILER_FULL} and nvcc (${CUDA_VERSION}) for cuda arch \\\"sm_${SPIRIT_CUDA_ARCH}\\\"" )
-
-elseif( "${CMAKE_CXX_COMPILER_ID}" MATCHES "NVHPC") 
-    set( CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -cuda" )
-
-    set( META_COMPILER         "${META_COMPILER}" )
-    set( META_COMPILER_VERSION "${META_COMPILER_VERSION} and ${CUDA_VERSION}" )
-    set( META_COMPILER_FULL    "${META_COMPILER_FULL} and nvcc (${CUDA_VERSION}) for cuda arch \\\"sm_${SPIRIT_CUDA_ARCH}\\\"" )
-
-else()
-    set( CMAKE_CUDA_ARCHITECTURES ${SPIRIT_CUDA_ARCH} )
-    set( META_COMPILER         "${META_COMPILER}" )
-    set( META_COMPILER_VERSION "${META_COMPILER_VERSION} and CUDA ${CUDA_VERSION}" )
-    set( META_COMPILER_FULL    "${META_COMPILER_FULL} and (CUDA ${CUDA_VERSION}) for cuda arch \\\"${SPIRIT_CUDA_ARCH}\\\"" )
+if( NOT DEFINED CMAKE_CUDA_ARCHITECTURES )
+    set( CMAKE_CUDA_ARCHITECTURES ${SPIRIT_CUDA_ARCHITECTURES} )
 endif()
+
+set( _CUDA_OPTS "" )
+
+if( "${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA" )
+    string(APPEND _CUDA_OPTS
+        "-lineinfo;"
+        "--expt-relaxed-constexpr;"
+        "--expt-extended-lambda;"
+        "--diag-suppress=20011;"
+        "--diag-suppress=20012;"
+        "--diag-suppress=20014;"
+        "--diag-suppress=1675;"
+        "--display-error-number;"
+    )
+elseif( "${CMAKE_CXX_COMPILER_ID}" MATCHES "NVHPC") 
+    string(APPEND _CUDA_OPTS
+        "-cuda;"
+    )
+endif()
+
+set( META_COMPILER         "${META_COMPILER}" )
+set( META_COMPILER_VERSION "${META_COMPILER_VERSION} and CUDA ${CUDA_VERSION}" )
+set( META_COMPILER_FULL    "${META_COMPILER_FULL} and (CUDA ${CUDA_VERSION}) for cuda arch \\\"${CMAKE_CUDA_ARCHITECTURES}\\\"" )
+
+add_compile_options("$<$<COMPILE_LANGUAGE:CUDA>:${_CUDA_OPTS}>")
 
 if( NOT DEFINED CMAKE_CUDA_STANDARD )
     set( CMAKE_CUDA_STANDARD 17 )
@@ -35,5 +38,5 @@ if( NOT DEFINED CMAKE_CUDA_STANDARD )
 endif()
 
 message( STATUS ">> Using CUDA. Flags: ${CMAKE_CUDA_FLAGS}" )
-message( STATUS ">> CUDA toolkit path: ${CUDA_TOOLKIT_ROOT_DIR}" )
+message( STATUS ">> CUDA toolkit path: ${CUDAToolkit_LIBRARY_ROOT}" )
 message( STATUS ">> CUDA libraries: ${CUDA_LIBRARIES}" )
